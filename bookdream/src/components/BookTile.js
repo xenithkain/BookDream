e4c22a1e2e217bd884a9b1405896fbdcaff32431
XKain1117 <cebesser1117@hotmail.com>
2024-09-21 17:12:03 -0700

    Made book list look better, same with scan modal.

--- bookdream/src/components/BookTile.js
+++ bookdream/src/components/BookTile.js
@@ -1,15 +1,21 @@
-import Book from "../components/Book";
+import Book from '../components/Book'
 
-function BookTile({ book }) {
+function BookTile ({ book }) {
   return (
     <>
-      <div className="BookTileContainer">
-        <img src={book.getCover("Medium")} alt="Book Cover"></img>
-        <h5>{book.title}</h5>
-        <h5>{book.authors}</h5>
+      <div className='BookTileContainer'>
+        <img src={book.getCover('Medium')} alt='Book Cover'></img>
+        <div className='BookTileInformation'>
+          <p>
+            {book.title.length < 40
+              ? book.title
+              : book.title.slice(0, 40) + '...'}
+          </p>
+          <p>{book.authors[0]}</p>
+        </div>
       </div>
     </>
-  );
+  )
 }
 
-export default BookTile;
+export default BookTile
